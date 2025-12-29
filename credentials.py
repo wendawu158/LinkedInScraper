8b779696922b543454d975663512eb4ed70f8e92
Wenda Wu <wendawu3011@gmail.com>
2025-12-29 00:22:27 +0000

    prototype

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# Remove this line (you don't get my credentials lol)
+import WendaCredentials
+
+# Add your credentials here
+username = WendaCredentials.username
+password = WendaCredentials.password
